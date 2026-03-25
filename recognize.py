086a413afe83960c853692d08b0e89479c3407d7
trisha032 <trishachatterjee41@gmail.com>
2026-03-25 13:14:50 +0530

    Add attendance tracking functionality and initial attendance data

--- recognize.py
+++ recognize.py
@@ -1,63 +1,76 @@
+from ast import Name
+
 import cv2
 import os
+import pandas as pd
+from datetime import datetime
 from deepface import DeepFace
 
 DATASET_PATH = "dataset"
 TEMP_IMAGE = "temp.jpg"
-
+ATTENDANCE_FILE = "attendance/attendance.csv"
 
 def recognize_face():
-    name = "Unknown"
+ for person in os.listdir(DATASET_PATH):
+  person_folder = os.path.join(DATASET_PATH, person)
 
-    for person in os.listdir(DATASET_PATH):
-        person_folder = os.path.join(DATASET_PATH, person)
+ for image in os.listdir(person_folder):
+        db_image = os.path.join(person_folder, image)
 
-        for image in os.listdir(person_folder):
-            db_image = os.path.join(person_folder, image)
+        result = DeepFace.verify(
+            img1_path=TEMP_IMAGE,
+            img2_path=db_image,
+            enforce_detection=False
+        )
 
-            result = DeepFace.verify(
-                img1_path=TEMP_IMAGE,
-                img2_path=db_image,
-                enforce_detection=False
-            )
+        if result["verified"]:
+            return person
 
-            if result["verified"]:
-                return person
+ return "Unknown"
 
-    return name
+def mark_attendance(name):
+    now = datetime.now()
 
+    date = now.strftime("%Y-%m-%d")
+    time = now.strftime("%H:%M:%S")
 
+    df = pd.read_csv(ATTENDANCE_FILE)
+
+    if name not in df["Name"].values:
+        df.loc[len(df)] = [name, date, time]
+        df.to_csv(ATTENDANCE_FILE, index=False)
+
+        print("Attendance marked for", name)
 def start_camera():
-    print("Starting camera... Press ESC to exit")
+ print("Starting camera... Press ESC to exit")
 
-    cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
-    while True:
-        ret, frame = cap.read()
+while True:
+    ret, frame = cap.read()
 
-        if not ret:
-            print("Camera error")
-            break
+    if not ret:
+        print("Camera error")
+        break
 
-        cv2.imwrite(TEMP_IMAGE, frame)
+    cv2.imwrite(TEMP_IMAGE, frame)
 
-        try:
-            name = recognize_face()
-        except:
-            name = "Unknown"
+    name = recognize_face()
 
-        cv2.putText(frame, name, (50, 50),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    1, (0, 255, 0), 2)
+    if name != "Unknown":
+        mark_attendance(name)
 
-        cv2.imshow("Smart Attendance Camera", frame)
+    cv2.putText(frame, name, (50, 50),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1, (0, 255, 0), 2)
 
-        if cv2.waitKey(1) == 27:
-            break
+    cv2.imshow("Smart Attendance Camera", frame)
 
-    cap.release()
-    cv2.destroyAllWindows()
+    if cv2.waitKey(1) == 27:
+        break
 
+cap.release()
+cv2.destroyAllWindows()
 
-if __name__ == "__main__":
-    start_camera()
+if name == "main":
+ start_camera()
